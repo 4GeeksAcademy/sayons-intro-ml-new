c6adf48dc96618c4b1823b86d0f1aa03aedc34e1
sayons <simanpito@gmail.com>
2025-07-03 02:23:04 +0200

    linear-regression-reg[wip]

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,21 @@
-from dotenv import load_dotenv
-from sqlalchemy import create_engine
 import pandas as pd
+from sklearn.metrics import mean_squared_error, root_mean_squared_error
+from sklearn.metrics import median_absolute_error, mean_absolute_percentage_error
+from sklearn.metrics import r2_score
 
-# load the .env file variables
-load_dotenv()
 
-
-def db_connect():
-    import os
-    engine = create_engine(os.getenv('DATABASE_URL'))
-    engine.connect()
-    return engine
+def get_metrics(y_predict_test, y_test, y_predict_train, y_train):
+    metrics_train = (r2_score(y_train, y_predict_train),
+                     median_absolute_error(y_train, y_predict_train),
+                     mean_absolute_percentage_error(y_train, y_predict_train) * 100,
+                     mean_squared_error(y_train, y_predict_train),
+                     root_mean_squared_error(y_train, y_predict_train))
+    metrics_test = (r2_score(y_test, y_predict_test),
+                    median_absolute_error(y_test, y_predict_test),
+                    mean_absolute_percentage_error(y_test, y_predict_test) * 100,
+                    mean_squared_error(y_test, y_predict_test),
+                    root_mean_squared_error(y_test, y_predict_test))
+    metrics_diff = list(map(lambda x: x[1] - x[0], zip(metrics_train, metrics_test)))
+    return pd.DataFrame(data=[metrics_train, metrics_test, metrics_diff],
+                        columns=['R2', 'MAE', 'MAPE', 'MSE', 'RMSE'],
+                        index=['Train set', 'Test set', 'Difference'])
