a5bae107b0411f7a0c6e870b7db9967cc32052ba
sayons <simanpito@gmail.com>
2025-07-05 18:01:55 +0200

    decision-tree[wip]

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,16 @@
 import pandas as pd
+# Regression Metrics
 from sklearn.metrics import mean_squared_error, root_mean_squared_error
 from sklearn.metrics import median_absolute_error, mean_absolute_percentage_error
 from sklearn.metrics import r2_score
+# Classifier Metrics
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
 
 
-def get_metrics(y_predict_test, y_test, y_predict_train, y_train):
+def get_regression_metrics(y_predict_test, y_test, y_predict_train, y_train):
     metrics_train = (r2_score(y_train, y_predict_train),
                      median_absolute_error(y_train, y_predict_train),
                      mean_absolute_percentage_error(y_train, y_predict_train) * 100,
@@ -19,3 +25,18 @@ def get_metrics(y_predict_test, y_test, y_predict_train, y_train):
     return pd.DataFrame(data=[metrics_train, metrics_test, metrics_diff],
                         columns=['R2', 'MAE', 'MAPE', 'MSE', 'RMSE'],
                         index=['Train set', 'Test set', 'Difference'])
+
+
+def get_classifier_metrics(y_predict_test, y_test, y_predict_train, y_train, average='micro'):
+    metrics_train = (accuracy_score(y_train, y_predict_train),
+                     f1_score(y_train, y_predict_train, average=average),
+                     precision_score(y_train, y_predict_train, average=average),
+                     recall_score(y_train, y_predict_train, average=average))
+    metrics_test = (accuracy_score(y_test, y_predict_test),
+                    f1_score(y_test, y_predict_test, average=average),
+                    precision_score(y_test, y_predict_test, average=average),
+                    recall_score(y_test, y_predict_test, average=average))
+    return pd.DataFrame(data=[metrics_train, metrics_test],
+                        columns=['Accuracy', 'F1 Score', 'Precision', 'Recall'],
+                        index=['Train set', 'Test set'])
+
